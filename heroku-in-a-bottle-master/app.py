024e95ff2d5b3121766806f12760bd856bcc20a9
noobay <shadowswatcher@gmail.com>
2016-12-02 23:58:35 +0200

    testing new parse

--- heroku-in-a-bottle-master/app.py
+++ heroku-in-a-bottle-master/app.py
@@ -7,17 +7,20 @@ from sys import argv
 
 import bottle
 from bottle import default_app, request, route, response, get
+global urlList 
+urlList = ""
 
 @get('/')
 def index():
     response.content_type = 'text/plain; charset=utf-8'
-    return urlList
-
+    return ret
 def onInit():
     global urlList
+
+    
     f = open('urllist.txt', 'r')
     urlString = f.read()
-    urlList = urllist.split('\n');
+    urlList = urlList.split('\n');
     
 onInit()
 bottle.run(host='0.0.0.0', port=argv[1])
